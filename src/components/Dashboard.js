0b8a9668bd8342145111dc0bb2cd6d1bdecd8092
Samantha Jeet <samanthajeet@gmail.com>
2020-03-28 00:16:01 -0600

    refactor backend

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Generate from './Generator'
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Generate />
+        </div>
+    )
+}
+
+
+export default Dashboard;
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom';
 import Contribute from './components/Contribute';
-import Generator from './components/Generator';
+import Dashbaord from './components/Dashboard';
 
 export default (
   <Switch>
-    <Route path='/' exact component={Generator} />
+    <Route path='/' exact component={Dashbaord} />
     <Route path='/contribute' exact component={Contribute} />
   </Switch>
 )
